2961bbd855228d2eb8c703b44bba6ce6629021c0
GojeeApps <jack@ducats.net.au>
2020-04-30 14:11:23 +1000

    Adding test harness

--- test/poker_problem/core_test.clj
+++ test/poker_problem/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [poker-problem.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest best-hands
+  (testing "Make sure the problem hands all produce same result"
+    (is (= :high-card (best-hand ["HA" "D2" "H3" "C9" "DJ"])))
+    (is (= :pair (best-hand ["HA" "HQ" "SJ" "DA" "HT"])))
+    (is (= :two-pair (best-hand ["HA" "DA" "HQ" "SQ" "HT"])))
+    (is (= :three-of-a-kind (best-hand ["HA" "DA" "CA" "HJ" "HT"])))
+    (is (= :straight (best-hand ["HA" "DK" "HQ" "HJ" "HT"])))
+    (is (= :straight (best-hand ["HA" "H2" "S3" "D4" "C5"])))
+    (is (= :flush (best-hand ["HA" "HK" "H2" "H4" "HT"])))
+    (is (= :full-house (best-hand ["HA" "DA" "CA" "HJ" "DJ"])))
+    (is (= :four-of-a-kind (best-hand ["HA" "DA" "CA" "SA" "DJ"])))
+    (is (= :straight-flush (best-hand ["HA" "HK" "HQ" "HJ" "HT"])))))
